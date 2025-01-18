745298b5fb3cfd7fc7917fb3da428010ca63d43a
Sudeep U <sudeepu@Sudeeps-MacBook-Air.local>
2025-01-19 00:18:29 +0530

    update

---
+++ SpringbootWebTutorial/src/main/java/com/codingShuttle/SpringbootWebtutorial/annotations/EmployeeRoleValidation.java
@@ -0,0 +1,4 @@
+package com.codingShuttle.SpringbootWebtutorial.annotations;
+
+public class EmployeeRoleValidation {
+}
---
+++ SpringbootWebTutorial/src/main/java/com/codingShuttle/SpringbootWebtutorial/annotations/EmployeeRoleValidator.java
@@ -0,0 +1,4 @@
+package com.codingShuttle.SpringbootWebtutorial.annotations;
+
+public class EmployeeRoleValidator {
+}
